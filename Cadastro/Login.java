15deb54a8fb66cea31dbe9c155cd8dbf7c37884e
Camila <camila.matosrocha1234@gmail.com>
2022-05-17 09:01:02 -0300

    Projeto Bloco 1 terminado!

---
+++ Cadastro/Login.java
@@ -0,0 +1,77 @@
+package Cadastro;
+
+public class Login {
+
+	private String nome;
+	private String senha;
+	
+	public Login (String nome, String senha)  { 
+		this.nome = nome;
+		this.senha = senha;
+	}
+	
+	
+	// GETTERS E SETTERS
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getSenha() {
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	
+	// M�TODOS
+	
+	public void menuCadastro() {
+		System.out.print("##--PIZZARIA GENERATION BRASIL--##\n\n");
+        System.out.print("🍕|-------------[Login]-------------🍕|\n");
+        System.out.print("🍕|[1] - Login                      🍕|\n");
+        System.out.print("🍕|[2] - Cadastro                   🍕|\n");
+        System.out.print("🍕|[3] - Fazer Pedido sem Cadastro  🍕|\n");
+        System.out.print("🍕|---------------------------------🍕|\n");
+        System.out.println();
+        System.out.print("OBS: A OPÇÃO [3] BLOQUEIA A OPÇÃO DE DELIVERY\n");
+        System.out.println();
+        System.out.print("\nDigite uma opção: ");
+	}
+	
+	public void telaAdeus () {
+		System.out.println("        r==\r\n"
+    			+ "        _  //\r\n"
+    			+ "       |_)//(''''':\r\n"
+    			+ "         //  \\_____:_____.-----.P\r\n"
+    			+ "        //   | ===  |   /        \\\r\n"
+    			+ "    .:'//.   \\ \\=|   \\ /  .:'':.\r\n"
+    			+ "   :' // ':   \\ \\ ''..'--:'-.. ':\r\n"
+    			+ "   '. '' .'    \\:.....:--'.-'' .'\r\n"
+    			+ "    ':..:'      ATÉ LOGO    ':..:'\n");
+	}
+	
+	public void telaAdeus (String mensagem) {
+		mensagem = "Sua pizza está a caminho! 🏍️💨";
+		
+		System.out.println("        r==\r\n"
+    			+ "        _  //\r\n"
+    			+ "       |_)//(''''':\r\n"
+    			+ "         //  \\_____:_____.-----.P\r\n"
+    			+ "        //   | ===  |   /        \\\r\n"
+    			+ "    .:'//.   \\ \\=|   \\ /  .:'':.\r\n"
+    			+ "   :' // ':   \\ \\ ''..'--:'-.. ':\r\n"
+    			+ "   '. '' .'    \\:.....:--'.-'' .'\r\n"
+    			+ "    ':..:'      BEEP BEEP  ':..:'\n"
+    			+ mensagem);
+	}
+
+
+	public void setEndereco(String next) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+}
---
+++ InfosPagamento/NotaFiscal.java
@@ -0,0 +1,43 @@
+package InfosPagamento;
+
+public class NotaFiscal {
+
+	public NotaFiscal (String endereco, int opcaoLogin, int tempoEntrega, double valorTotalPedido) {
+
+		if (opcaoLogin == 1 || opcaoLogin==2 ) {
+			System.out.println(
+					 "\n=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-="
+					+"\n       📜 INFORMAÇÕES DA ENTREGA 📜"
+					+"\nENDEREÇO DE ENTREGA: \n" + endereco
+					+"\nOPÇÃO DE DELIVERY: ENTREGA" 
+					+"\nTempo de Entrega: " + tempoEntrega + " min"
+					+"\n=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-="
+					+" \n       📜 INFORMAÇÕES DO PEDIDO 📜"
+					+"\n"
+					+"\nVALOR DELIVERY R$ 10"
+					+"\nVALOR DO PEDIDO R$:" + valorTotalPedido 
+					+"\nVALOR FINAL R$" + (valorTotalPedido+10)
+					+ "\n=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
+				
+			
+			
+		} else {
+			System.out.println(
+					 "\n=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-="
+					+"\n       📜 INFORMAÇÕES DA ENTREGA 📜"
+					+"\nENDEREÇO DE ENTREGA: \n" + endereco
+					+"\nOPÇÃO DE DELIVERY: BUSCAR NO ESTABELECIMENTO" 
+					+"\nTempo de Entrega: " + tempoEntrega + " min"
+					+"\n=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-="
+					+"\n       📜 INFORMAÇÕES DO PEDIDO 📜"
+					+"\n"
+					+"\nVALOR DELIVERY R$ 0,00"
+					+"\nVALOR DO PEDIDO R$:" + valorTotalPedido 
+					+"\nVALOR FINAL R$" + valorTotalPedido
+					+ "\n=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
+				
+		}
+		
+	}
+
+}
